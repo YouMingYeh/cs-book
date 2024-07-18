710e15e8d8a88a5ef0b440d4e7805009cb244933
YouMingYeh <ym911216@gmail.com>
2024-07-19 02:52:43 +0800

    first commit

---
+++ next.config.js
@@ -0,0 +1,18 @@
+const isProd = process.env.NODE_ENV === "production";
+
+const withNextra = require("nextra")({
+  theme: "nextra-theme-docs",
+  themeConfig: "./theme.config.tsx",
+});
+
+const nextConfig = {
+  basePath: isProd ? "/docs" : "",
+  assetPrefix: "./",
+  reactStrictMode: true,
+  output: isProd ? "export" : "standalone",
+  distDir: "dist",
+  images: {
+    unoptimized: true,
+  },
+};
+module.exports = withNextra(nextConfig);
---
+++ theme.config.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { DocsThemeConfig } from 'nextra-theme-docs'
+
+const config: DocsThemeConfig = {
+  logo: <span>上田鋁業鋁門窗生產規劃專案</span>,
+  // project: {
+  //   link: 'https://github.com/shuding/nextra-docs-template',
+  // },
+  // chat: {
+  //   link: 'https://discord.com',
+  // },
+  // docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
+  footer: {
+    text: '臺大資管孔令傑教授專題學生團隊',
+  },
+}
+
+export default config
