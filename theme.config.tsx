41134886869dfdd0e35af05b0b81497677738439
YouMingYeh <ym911216@gmail.com>
2024-07-19 05:28:26 +0800

    chore: Add new pages and update styles

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,18 +1,38 @@
-import React from 'react'
-import { DocsThemeConfig } from 'nextra-theme-docs'
+import React from "react";
+import { DocsThemeConfig } from "nextra-theme-docs";
 
 const config: DocsThemeConfig = {
-  logo: <span>上田鋁業鋁門窗生產規劃專案</span>,
-  // project: {
-  //   link: 'https://github.com/shuding/nextra-docs-template',
-  // },
+  logo: (
+    <span
+      style={{
+        fontWeight: "bold",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        gap: "0.5em",
+      }}
+    >
+      <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="lucide lucide-book-open-text"><path d="M2 3h6a4 4 0 0 1 4 4v14a3 3 0 0 0-3-3H2z"/><path d="M22 3h-6a4 4 0 0 0-4 4v14a3 3 0 0 1 3-3h7z"/><path d="M6 8h2"/><path d="M6 12h2"/><path d="M16 8h2"/><path d="M16 12h2"/></svg>
+      CS Book
+    </span>
+  ),
+  project: {
+    link: "https://github.com/YouMingYeh/cs-book.git",
+  },
   // chat: {
   //   link: 'https://discord.com',
   // },
-  // docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
+  docsRepositoryBase: 'https://github.com/YouMingYeh/cs-book.git',
   footer: {
-    text: '臺大資管孔令傑教授專題學生團隊',
+    text: "Created by YouMingYeh",
   },
-}
+  head: (
+    <>
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <meta property="og:title" content="CS Book" />
+      <meta property="og:description" content="A book for learning computer science" />
+    </>
+  ),
+};
 
-export default config
+export default config;
