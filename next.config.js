188b28f8f4e856326c8954fe06aba3506711c4bb
YouMingYeh <ym911216@gmail.com>
2024-07-19 15:02:03 +0800

    chore: Add exercises for operating system and update linear algebra page

--- next.config.js
+++ next.config.js
@@ -3,11 +3,12 @@ const isProd = process.env.NODE_ENV === "production";
 const withNextra = require("nextra")({
   theme: "nextra-theme-docs",
   themeConfig: "./theme.config.tsx",
+  latex: true,
 });
 
 const nextConfig = {
   basePath: isProd ? "/cs-book" : "",
-  assetPrefix: "./",
+  assetPrefix: isProd ? "./" : undefined,
   reactStrictMode: true,
   output: isProd ? "export" : "standalone",
   distDir: "dist",
